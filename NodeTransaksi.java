cc77553c6cc527d0a637347b49c75548136a8ab4
Fardhan Ardhi <fa.dsg.id@gmail.com>
2018-06-12 17:39:16 +0700

    Update transaksi node

--- NodeTransaksi.java
+++ NodeTransaksi.java
@@ -1,5 +1,17 @@
 package sppPayment;
 
 public class NodeTransaksi {
-    
+    NodeSiswa siswa;
+    int jumlahBayar;
+    int jumlahBulan;
+    NodeTransaksi prev;
+    NodeTransaksi next;
+
+    NodeTransaksi(NodeTransaksi prev, NodeSiswa siswa, int jumlahBayar, int jumlahBulan, NodeTransaksi next) {
+        this.prev = prev;
+        this.siswa = siswa;
+        this.jumlahBayar = jumlahBayar;
+        this.jumlahBulan = jumlahBulan;
+        this.next = next;
+    }
 }
